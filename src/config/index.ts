0a607bf74e6b5bc749c8756013e1065a12368312
jamunarengaraj <120617668+jamunarengaraj@users.noreply.github.com>
2023-09-12 11:52:21 +0530

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,18 @@
+//-----FOLLOW THIS TYPE OF FILE STRUCTURE. IF THERE IS NEED TO ADD ANYONEONE HOST, ADD BELOW TEAM-LOCALHOST.
+// DO NOT CHANGE THE URL ABOVE TEAM-LOCALHOST
+//---LOCALHOST
+// export const API_BASE_URL = "http://localhost:1337"; //local-server
+//
+//---DEVELOPMENT
+// export const API_BASE_URL = "http://3.109.230.255:1337"; //dev-server
+
+//QA
+export const API_BASE_URL = 'http://10.10.10.76:5000'; //qa-server
+
+//---PRODUCTION
+// export const API_BASE_URL = "https://nvplerp.nanoventions.in/api"; //stage-server
+
+//---TEAM-LOCALHOST
+// export const API_BASE_URL = "http://192.168.0.150:1337"; //ip00
+// export const API_BASE_URL = "http://172.20.10.9:1337"; //ip
+// export const API_BASE_URL = "http://10.10.10.221:1337"; //ip
